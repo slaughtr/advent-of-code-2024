eeb2ad892833b1d7aa02c7355654825dd944d312
Slaughter <slaughter@hey.com>
2024-12-03 08:21:33 -0800

    day 1 complete in python and swift. hello me in the future hating this git practice

---
+++ days/1/swift/1.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+let inputFile = "input.txt"
+
+func parseInput(fromFile file: String) -> ([Int], [Int]) {
+    print("Trying to parse that input file teehee")
+
+    var leftCol: [Int] = []
+    var rightCol: [Int] = []
+
+    do {
+        let contents: String = try String(contentsOfFile: file)
+        let lines = contents.components(separatedBy: .newlines)
+
+        for line in lines {
+            // 3 spaces separates left and right col in the input
+            let cols: [String] = line.components(separatedBy: "   ")
+            if let left = Int(cols[0]), let right = Int(cols[1]) {
+                leftCol.append(left)
+                rightCol.append(right)
+            }
+        }
+    } catch {
+        print("Error loading input file")
+    }
+    
+    let sortedLeftCol = leftCol.sorted()
+    let sortedRightCol = rightCol.sorted() 
+
+    return (sortedLeftCol, sortedRightCol)
+}
+
+let (leftCol, rightCol) = parseInput(fromFile: inputFile)
+
+var numbers = Dictionary(uniqueKeysWithValues: zip(leftCol, rightCol))
+var total: Int = 0
+for (left, right) in numbers {
+    let diff = abs(left - right)
+    total += diff
+}
+
+print("part one total: \(total)")
+
+// part two
+
+let columns = [leftCol, rightCol]
+var occurences: [Int: Int] = Dictionary(minimumCapacity: 10000)
+var partTwoTotal: Int = 0
+
+for left in leftCol {
+    // print("left: \(left), current: \(current), next: \(next)")
+    occurences.updateValue(0, forKey: left)
+    for right in rightCol {
+        if right == left {
+            let current: Int? = occurences[left]
+            let next = current! + 1
+            occurences.updateValue(next, forKey: left)
+        }
+    }
+}
+
+for (left, _) in occurences {
+   partTwoTotal += (left * occurences[left]!) 
+}
+
+print(partTwoTotal)
+
